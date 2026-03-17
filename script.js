3c47a1cd8a1757582069bd410bf34a012c1e6b9b
Angello Reyes <maurooo.reyes777gmail.com>
2026-03-16 21:59:11 -0600

    Agregue archivos de Dulcenea Pop

---
+++ script.js
@@ -0,0 +1,10 @@
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener("click", function() {
+
+    const type = password.getAttribute("Type") === "password" ? "text" : "password";
+
+    password.setAttribute("type" , type);
+
+});
